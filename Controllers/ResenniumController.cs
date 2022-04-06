1cf2fd4e7ecf47dbd181e672b35662b477db53a5
KJ_27 <cespedeskendall@hotmail.com>
2022-04-05 18:07:46 -0600

    Views - Added
    Pending triggers and Login->Cliente

---
+++ Controllers/ResenniumController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using webHibiscus.Models;
+
+namespace webHibiscus.Controllers
+{
+    public class ResenniumController : Controller
+    {
+        private readonly db_a83ea8_hibiscusadminContext _context;
+
+        public ResenniumController(db_a83ea8_hibiscusadminContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resennium
+        public async Task<IActionResult> Index()
+        {
+            var db_a83ea8_hibiscusadminContext = _context.Resennia.Include(r => r.IdClienteNavigation);
+            return View(await db_a83ea8_hibiscusadminContext.ToListAsync());
+        }
+
+        // GET: Resennium/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resennium = await _context.Resennia
+                .Include(r => r.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.IdResenia == id);
+            if (resennium == null)
+            {
+                return NotFound();
+            }
+
+            return View(resennium);
+        }
+
+        // GET: Resennium/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1");
+            return View();
+        }
+
+        // POST: Resennium/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdResenia,Cotenido,IdCliente,Puntuacion,Fecha")] Resennium resennium)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resennium);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", resennium.IdCliente);
+            return View(resennium);
+        }
+
+        // GET: Resennium/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resennium = await _context.Resennia.FindAsync(id);
+            if (resennium == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", resennium.IdCliente);
+            return View(resennium);
+        }
+
+        // POST: Resennium/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdResenia,Cotenido,IdCliente,Puntuacion,Fecha")] Resennium resennium)
+        {
+            if (id != resennium.IdResenia)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resennium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResenniumExists(resennium.IdResenia))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", resennium.IdCliente);
+            return View(resennium);
+        }
+
+        // GET: Resennium/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resennium = await _context.Resennia
+                .Include(r => r.IdClienteNavigation)
+                .FirstOrDefaultAsync(m => m.IdResenia == id);
+            if (resennium == null)
+            {
+                return NotFound();
+            }
+
+            return View(resennium);
+        }
+
+        // POST: Resennium/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resennium = await _context.Resennia.FindAsync(id);
+            _context.Resennia.Remove(resennium);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResenniumExists(int id)
+        {
+            return _context.Resennia.Any(e => e.IdResenia == id);
+        }
+    }
+}
---
+++ Controllers/ReservaController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using webHibiscus.Models;
+
+namespace webHibiscus.Controllers
+{
+    public class ReservaController : Controller
+    {
+        private readonly db_a83ea8_hibiscusadminContext _context;
+
+        public ReservaController(db_a83ea8_hibiscusadminContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reserva
+        public async Task<IActionResult> Index()
+        {
+            var db_a83ea8_hibiscusadminContext = _context.Reservas.Include(r => r.IdClienteNavigation).Include(r => r.IdServicioNavigation);
+            return View(await db_a83ea8_hibiscusadminContext.ToListAsync());
+        }
+
+        // GET: Reserva/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reservas
+                .Include(r => r.IdClienteNavigation)
+                .Include(r => r.IdServicioNavigation)
+                .FirstOrDefaultAsync(m => m.IdReserva == id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // GET: Reserva/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1");
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre");
+            return View();
+        }
+
+        // POST: Reserva/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdReserva,FechaHora,IdCliente,IdServicio")] Reserva reserva)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reserva);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", reserva.IdCliente);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", reserva.IdServicio);
+            return View(reserva);
+        }
+
+        // GET: Reserva/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reservas.FindAsync(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", reserva.IdCliente);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", reserva.IdServicio);
+            return View(reserva);
+        }
+
+        // POST: Reserva/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdReserva,FechaHora,IdCliente,IdServicio")] Reserva reserva)
+        {
+            if (id != reserva.IdReserva)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reserva);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservaExists(reserva.IdReserva))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCliente"] = new SelectList(_context.Clientes, "IdCliente", "Apellido1", reserva.IdCliente);
+            ViewData["IdServicio"] = new SelectList(_context.Servicios, "IdServicio", "Nombre", reserva.IdServicio);
+            return View(reserva);
+        }
+
+        // GET: Reserva/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reserva = await _context.Reservas
+                .Include(r => r.IdClienteNavigation)
+                .Include(r => r.IdServicioNavigation)
+                .FirstOrDefaultAsync(m => m.IdReserva == id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return View(reserva);
+        }
+
+        // POST: Reserva/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reserva = await _context.Reservas.FindAsync(id);
+            _context.Reservas.Remove(reserva);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservaExists(int id)
+        {
+            return _context.Reservas.Any(e => e.IdReserva == id);
+        }
+    }
+}
